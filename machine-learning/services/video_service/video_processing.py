82bcb5a86370c42fcfcb9d7ba05d90e02c2d83bd
UsusCimex <d.lanin@g.nsu.ru>
2024-06-16 23:55:00 +0700

    edit services

--- machine-learning/services/video_service/video_processing.py
+++ machine-learning/services/video_service/video_processing.py
@@ -1,14 +1,17 @@
+import sys
 import os
 import json
 import asyncio
 import aiohttp
 import numpy as np
 from PIL import Image
-from aiokafka import AIOKafkaProducer, AIOKafkaConsumer
-import moviepy.editor as mp
+from aiokafka import AIOKafkaConsumer
+sys.path.append('..')
+from kafka_utils import send_to_kafka, download_file
 from tensorflow.keras.applications.efficientnet import EfficientNetB7, preprocess_input, decode_predictions
 from tensorflow.keras.preprocessing import image
 import tensorflow as tf
+import moviepy.editor as mp
 
 # Настройка GPU
 physical_devices = tf.config.list_physical_devices('GPU')
@@ -16,13 +19,9 @@ if len(physical_devices) > 0:
     tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 # Настройка Kafka
-KAFKA_BROKER = 'localhost:9092'
+KAFKA_BROKER = 'kafka:29092'
 VIDEO_TOPIC = 'video_topic'
 RESULT_TOPIC = 'result_topic'
-
-loop = asyncio.get_event_loop()
-producer = AIOKafkaProducer(loop=loop, bootstrap_servers=KAFKA_BROKER)
-consumer = AIOKafkaConsumer(VIDEO_TOPIC, loop=loop, bootstrap_servers=KAFKA_BROKER, group_id="video_group")
 
 # Загрузка модели
 model = EfficientNetB7(weights='imagenet')
@@ -48,23 +47,11 @@ def process_video_stream(video, input_shape, frame_interval=0.5):
         decoded_preds = predict_with_model(img_array)
         predictions.extend(decoded_preds)
 
-    result_dict = {}
-    for pred in predictions:
-        label = pred[1]
-        confidence = pred[2]
-        if label in result_dict:
-            result_dict[label] += confidence
-        else:
-            result_dict[label] = confidence
-
-    for label in result_dict:
-        result_dict[label] /= len(frames)
-
-    sorted_result = dict(sorted(result_dict.items(), key=lambda item: item[1], reverse=True))
-    res = [sr[0] for sr in sorted_result.items()]
-    return res
+    labels = {pred[1] for pred in predictions}
+    return list(labels)
 
 async def send_to_kafka(topic, data: dict):
+    producer = AIOKafkaProducer(bootstrap_servers=KAFKA_BROKER)
     await producer.start()
     try:
         value = json.dumps(data).encode('utf-8')
@@ -82,30 +69,39 @@ async def download_video(url, file_path):
             else:
                 raise Exception(f"Failed to download video. Status code: {response.status}")
 
+async def process_video_message(data, send_to_kafka, result_topic):
+    video_url = data.get('video_link')
+    if video_url:
+        video_file_path = f"temp/{os.path.basename(video_url)}"
+        await download_video(video_url, video_file_path)
+        
+        video = mp.VideoFileClip(video_file_path)
+        labels = process_video_stream(video, input_shape=(224, 224))
+        
+        result_data = {
+            "type": "video",
+            "video_link": video_url,
+            "marks": labels
+        }
+        
+        await send_to_kafka(result_topic, result_data)
+        
+        os.remove(video_file_path)
+
 async def consume():
+    consumer = AIOKafkaConsumer(VIDEO_TOPIC, bootstrap_servers=KAFKA_BROKER, group_id="video_group")
     await consumer.start()
     try:
         async for msg in consumer:
             data = json.loads(msg.value.decode('utf-8'))
-            video_url = data.get('vide_link')
-            
-            if video_url:
-                video_file_path = f"temp/{os.path.basename(video_url)}"
-                await download_video(video_url, video_file_path)
-                
-                video = mp.VideoFileClip(video_file_path)
-                result_dict = process_video_stream(video, input_shape=(224, 224))
-                
-                result_data = {
-                    "type": "video",
-                    "marks": result_dict
-                }
-                
-                await send_to_kafka(RESULT_TOPIC, result_data)
-                
-                os.remove(video_file_path)
+            await process_video_message(data, send_to_kafka, RESULT_TOPIC)
+    except Exception as e:
+        print(f"Kafka error: {e}")
     finally:
         await consumer.stop()
 
 if __name__ == "__main__":
-    loop.run_until_complete(consume())
+    try:
+        asyncio.run(consume())
+    except Exception as e:
+        print(f"Error running main loop: {e}")
